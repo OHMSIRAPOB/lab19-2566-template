7fb4e1c3cf8dec37d36950f228c7614781284fc6
ohmsirapob <fedfekfc@gmail.com>
2023-09-18 15:29:49 +0700

    succcess

--- src/app/page.js
+++ src/app/page.js
@@ -36,10 +36,12 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
@@ -54,14 +56,17 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      setLoadingLogin(false);
     } catch (error) {
       if (error.response.data) {
         alert(error.response.data.message);
+        setLoadingLogin(false);
       }
     }
   };
@@ -101,11 +106,14 @@ export default function Home() {
                 value={username}
               />
               <TextInput
+                type="password"
                 label="Password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              <Button disabled ={loadingLogin} onClick={login}> {loadingLogin ? "Login..." : "Login"} 
+              </Button>
+              
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +141,10 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {loadingMyCourses && (<Loader variant="dots" />)}
+          
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year="2023" fullName="Sirapob Lueangprasert" studentId="650612102" />
       </Stack>
     </Container>
   );
